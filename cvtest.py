fc6ba17f1761453461fe8878ac957b85831a4c30
Admin <magelan@tut.by>
2019-03-04 20:21:03 +0300

    first

---
+++ cvtest.py
@@ -0,0 +1,15 @@
+#test camera connect
+import numpy as np
+import cv2
+cap = cv2.VideoCapture(0)
+while(True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Video', frame)
+    cv2.imshow('Frame', gray)
+    #print(frame)
+    print(gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
